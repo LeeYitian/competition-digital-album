86c4b13118898c5081702516790a49f2404768f6
leeYitian <e2004emily@gmail.com>
2024-08-11 00:03:53 +0800

    init

---
+++ src/views/opening/Opening.tsx
@@ -0,0 +1,11 @@
+import { StyledBackground } from "./Opening.style";
+
+const Opening = () => {
+  return (
+    <StyledBackground>
+      <div>Opening</div>
+    </StyledBackground>
+  );
+};
+
+export default Opening;
